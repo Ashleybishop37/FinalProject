8879ab7a65da9acbb128b423adc1b36049758198
Ashley Bishop <ashley.bishop@meritenergy.com>
2020-11-26 01:47:22 -0600

    push1

---
+++ env/educa/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\abishop\desktop\school\e-comm_webdev\code\github\env\educa\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
